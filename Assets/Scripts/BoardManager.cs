e0884e3be4b42e608992b9728fa843fea4810cb9
crasnencov <crasnencov@gmail.com>
2018-10-31 22:55:25 +0200

    Created Board and Game scripts
    Board Manager, Game manager

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour {
+    [Serializable]
+    public class Count
+    {
+        public int maximum;
+        public int minimum;
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
